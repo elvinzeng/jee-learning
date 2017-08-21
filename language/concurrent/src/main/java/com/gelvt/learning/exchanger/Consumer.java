d4649489ece8bf761f81c31b47f92e4f5ee09914
Elvin Zeng <zenghui@188.com>
2017-08-21 19:33:23 +0800

    update sample 'concurrent'

--- language/concurrent/src/main/java/com/gelvt/learning/exchanger/Consumer.java
+++ language/concurrent/src/main/java/com/gelvt/learning/exchanger/Consumer.java
@@ -3,7 +3,6 @@ package com.gelvt.learning.exchanger;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.Exchanger;
 
 /**
